52542f2762c35c12d9370741d0e0626130d7f0d5
Raphael Loeffler <raphael.loeffler2010@gmail.com>
2025-03-12 19:12:11 +0100

    setup...

---
+++ modding-practice-template-1.21/src/main/java/de/raphael/moddingpractice/ModdingPracticeClient.java
@@ -0,0 +1,10 @@
+package de.raphael.moddingpractice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModdingPracticeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
